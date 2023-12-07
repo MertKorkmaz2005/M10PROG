0e2cdef445908aa5f00a0447f8a73d76db3364e3
Mert Korkmaz <mertkorkmaz_2005@hotmail.com>
2023-12-07 10:32:38 +0100

    les 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,18 +4,42 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link href="/dist/app.css" rel="stylesheet">
+        <title>GemeenteAmsterdam BMV</title>
+        <script src="https://kit.fontawesome.com/cb4527b064.js" crossorigin="anonymous"></script>
+    
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-       <style></style>
+        <link href="/dist/app.css" rel="stylesheet" />
     </head>
     <body>
-       
-    <div>Hallo</div>
+    <section class="home">
+      <header class="header">
+        <ul class="header__ul">
+          <div class="header__logowrapper">
+            <figure class="header__logo">
+              <img src="img/gemeente.png" alt="" class="header__logoimg" />
+            </figure>
+          </div>
+          <li class="header__li"> <a href="" class="header__link">Organisatie</a></li>
+          <li class="header__li"> <a href="" class="header__link">Projecten</a></li>
+          <li class="header__li"> <a href="" class="header__link">Werken bij</a></li>
+        </ul>
+      </header>
+      <div class="home__wrapperText">
+        <h2 class="home__h2">Wij bouwen met passie</h2>
+        <h3 class="home__h3">Creatief, deskundig en betrouwbaar</h3>
+      </div>
+      <figure class="home__buttonWrapper">
+        <a class="home__arrowLink" href=""
+          ><button class="home__arrowButton">
+            <i class=" home__arrowI fa-solid fa-arrow-down"></i>
+           </button
+        ></a>
+          </figure>
+    </section>
     </body>
 </html>
