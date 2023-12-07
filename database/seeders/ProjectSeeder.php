2c328dc1f72e1e4f422b4edbf25e4ce4b15b1158
Mert Korkmaz <mertkorkmaz_2005@hotmail.com>
2023-12-07 11:45:09 +0100

    les 4

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('projecten')->insert([
+            'titel'         => 'Mijn project titel',
+            'img'           => 'voorbeeld.img',
+            'video'         => 'voorbeeld.vid',
+            'description'   => 'Enim labore eu, sed. Sed esse incididunt aute velit. Incididunt, aute velit duis amet sint. Duis amet sint pariatur esse anim officia mollit. Sint pariatur esse anim. Esse anim officia mollit laboris aliqua, et esse.',
+            'active'        => true,
+        ]);
+    }
+}
+
--- resources/views/projecten.blade.php
+++ resources/views/projecten.blade.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>projecten</title>
+    <link href="/dist/app.css" rel="stylesheet" />
+
 </head>
 <body>
-    <h1>je projcten</h1>
+    <h1>je projecten</h1>
     
 </body>
 </html>
