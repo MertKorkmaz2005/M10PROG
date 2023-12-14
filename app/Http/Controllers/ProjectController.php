2ce260bf3caa3b7e854e207f7e3a03df115184bd
Mert Korkmaz <mertkorkmaz_2005@hotmail.com>
2023-12-14 11:01:04 +0100

    les 5

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index(){
         return view('projecten');
+    }
+
+    public function add() {
+        $model = new Project();
+        // definieer de velden
+
+        $model->titel = 'titel';
+        $model->img = 'https://images.pexels.com/photos/4456992/pexels-photo-4456992.jpeg?auto=compress&cs=tinysrgb&w=800';
+        $model->video = '//www.youtube.com/embed/wjTn_EkgQRg?si=tphj23V4wC3MLV9_';
+        $model->description = 'hallo ik ben je eerste project';
+        $model->active = 1;
+        // sla het model op
+        $model->save();
+        
     }
 }
--- resources/views/projecten.blade.php
+++ resources/views/projecten.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>projecten</title>
-    <link href="/dist/app.css" rel="stylesheet" />
+@extends( 'layouts.master' )
 
-</head>
-<body>
-    <h1>je projecten</h1>
-    
-</body>
-</html>
+@section('content')
+
+HOI K BEN MERT
+@endsection
+
+@section('scripts')
+        <script>
+            Console.log('Mijn script werkt');
+        </script>
+    @endsection
